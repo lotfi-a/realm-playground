b0efd7030617f6bdfe4b8e27a3c4c4c90d05e97e
lotfi <lotfi@lesfabricants.io>
2022-12-27 21:36:17 +0100

    ✨ Add fabby helper script to create a file function and editing the config.json

---
+++ fabby.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+const fs = require("fs");
+const functionPath = "./src/functions/";
+const configPath = "./src/functions/config.json";
+const args = process.argv.slice(2);
+const functionFileName = args[0];
+
+let functionConfig = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+fs.writeFile(
+  `${functionPath} ${functionFileName}.ts`,
+  `//${functionFileName} function file created by Fabby...`,
+  function (err) {
+    if (err) throw err;
+    console.log("Function file is created successfully.");
+  }
+);
+
+functionConfig.push({
+  name: functionFileName,
+  private: false,
+  runAsSystem: true,
+});
+
+fs.writeFileSync(
+  configPath,
+  JSON.stringify(functionConfig, null, 2),
+  function (err) {
+    if (err) throw err;
+    console.log("Config file is edited successfully.");
+  }
+);
