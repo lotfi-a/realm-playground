8df0c75806de20429309961c03ac55e95b5b0291
lotfi <lotfi@lesfabricants.io>
2022-12-27 21:55:32 +0100

    🔧 Add realm declaration type

---
+++ src/functions/types/realm.d.ts
@@ -0,0 +1,30 @@
+/// <reference types="mongodb" />
+type Services = {
+  (name: "mongodb-atlas"): import("mongodb").MongoClient;
+  // add your other services here
+  (name: string): any;
+};
+
+declare namespace context {
+  const services: {
+    get: Services;
+  };
+
+  const environment: {
+    tag: string;
+    values: Record<string, string>;
+  };
+  const request: {
+    rawQueryString: String;
+  };
+  const functions: {
+    execute: (name: string, ...args: any[]) => any;
+  };
+  const user: {
+    id: string;
+    type: "normal" | "server" | "system";
+    data: object;
+    custom_data: object;
+    identities: any[];
+  };
+}
